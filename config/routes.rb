8f75383f22409e52a569805df67403236dc7f273
justin <jvthibault@gmail.com>
2009-04-02 05:10:02 -0400

    Minor changes, tiny url

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.root :controller => "highlights", :action=>"new"
-  map.resources :highlights
+  map.resources :highlights, :as=>"h"
   map.resources :screenshots
-
+  map.connect ":id", :controller=>"highlights", :action=>"show", :id=>/[0-9]+/
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end

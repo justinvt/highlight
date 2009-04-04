ec172d00908bd3053b495cb23c7532a908ad70a6
justin <jvthibault@gmail.com>
2009-04-04 01:02:49 -0400

    auto

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,31 +1,32 @@
 require "rubygems"
 require "rwebthumb"
-#require 'builder'
+require 'httparty'
+require 'builder'
+require 'net/http'
+require 'rexml/document'
 
 class Screenshot < ActiveRecord::Base
   
   include Simplificator::Webthumb
+  include HTTParty
+
+  format :xml
   
   belongs_to :highlight
-  after_create :generate_tiny_url
   
   @@api_key = :dde49e22186a07b826e22770d1f78705
   @@user_id =  4461
+  @@api_endpoint = "http://webthumb.bluga.net/api.php"
   
     
   has_attachment :content_type => :image, 
-                   :storage => :s3, 
-                   :max_size => 4.megabytes,
-                   :processor=> :rmagick,
+                 :storage => :s3, 
+                 :max_size => 4.megabytes,
+                 :processor=> :rmagick,
               #   :resize_to => '700x400',
-                   :thumbnails => {:thumb => "c50x50" }
-                   
-  def generate_tiny_url
-    Highlight.make_tiny_url
-  end
-  
+                 :thumbnails => {:thumb => "c50x50" }
 
-  
+
   def self.add(highlight)
     filename = "#{RAILS_ROOT}/tmp/#{highlight.id}.jpg"
     thumb = Webthumb.new(@@api_key.to_s)
@@ -66,9 +67,71 @@ class Screenshot < ActiveRecord::Base
     File.delete(o.path)
   end
   
-  def request
+  def job_request
+%{<webthumb>
+    <apikey>#{@@api_key}</apikey>
+    <request>
+      <url>#{highlight.url.to_s}</url>
+      <fullthumb>1</fullthumb>
+    </request>
+</webthumb>}.gsub(/[\n \t\\]+/,'')
+end
+
+  def job_request
+%{<webthumb>
+    <apikey>#{@@api_key}</apikey>
+	  <fetch>
+		  <job>#{job_id}</job>
+		<size>full</size>
+	</fetch>
+</webthumb>}.gsub(/[\n \t\\]+/,'')
+end
+
+  def img_request
+%{<webthumb>
+    <apikey>#{@@api_key}</apikey>
+    <request>
+      <url>#{highlight.url.to_s}</url>
+      <fullthumb>1</fullthumb>
+  
+      <height>2048</height>
+    </request>
+</webthumb>}.gsub(/[\n \t\\]+/,'')
+end
+  
+  def request_components
+    {:webthumb => {
+          :apikey => @@api_key.to_s,
+          :request => {
+            :url => highlight.url.to_s,
+            :fullthumb => 1
+          }
+      }
+    }
+  end
+  
+  def full_size
+    self.class.post(@@api_endpoint, :query => request_components).inspect
+  end
+  
+  def via_curl(xml)
+    (IO.popen "curl -X POST #{@@api_endpoint} -d \"#{xml}\"").read
   end
 
-
+  def get_job_id
+    p = via_curl(img_request).gsub(/[\n \t\\]+/,'').scan(/[a-zA-Z0-9]+<\/job>/)[0].gsub(/<\/job>/,'')
+    update_attributes(:job_id=>p)
+  end
+  
+  def get_image
+   p =  via_curl(job_request)
+   File.open(File.join(RAILS_ROOT,'tmp','test.jpg'), 'w+').puts(p)
+  end
+  
+  def fetch_full
+    get_job_id
+    sleep 20
+    get_image
+  end
   
 end
--- db/migrate/20090401200939_create_screenshots.rb
+++ db/migrate/20090401200939_create_screenshots.rb
@@ -2,7 +2,7 @@ class CreateScreenshots < ActiveRecord::Migration
 
   def self.up
     create_table :screenshots do |t|
-      t.integer :parent_id, :highlight_id, :size, :width, :height
+      t.integer :parent_id, :highlight_id, :size, :width, :height, :progress, :job_id
       t.string :content_type, :filename, :thumbnail, :data_hash
     end
   end
